a21005184f2316e5f0796e24746eca92d522b6ed
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-02-24 13:43:23 +0000

    Task 0.1

--- _printf.c
+++ _printf.c
@@ -17,32 +17,38 @@ int _printf(const char *format, ...)
     va_start(args, format);
 
     while (format[i])
+{
+    if (format[i] == '%')
     {
-        if (format[i] == '%' && format[i + 1])
+        if (format[i + 1] == '\0')
         {
-            i++;
-
-            if (format[i] == 'c')
-                count += print_char(args);
-            else if (format[i] == 's')
-                count += print_string(args);
-            else if (format[i] == '%')
-                count += print_percent();
-            else
-            {
-                write(1, "%", 1);
-                write(1, &format[i], 1);
-                count += 2;
-            }
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            count++;
+            va_end(args);
+            return (-1);
         }
 
         i++;
+
+        if (format[i] == 'c')
+            count += print_char(args);
+        else if (format[i] == 's')
+            count += print_string(args);
+        else if (format[i] == '%')
+            count += print_percent();
+        else
+        {
+            write(1, "%", 1);
+            write(1, &format[i], 1);
+            count += 2;
+        }
     }
+    else
+    {
+        write(1, &format[i], 1);
+        count++;
+    }
+
+    i++;
+}
 
     va_end(args);
     return (count);
